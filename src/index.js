3c6bf7dd18dc98f0527ca371843b39c61f307400
karan <karansalunkhe88@gmail.com>
2021-03-21 09:29:23 +0530

    new props with css

--- src/index.js
+++ src/index.js
@@ -1,48 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
+ReactDOM.render(<App />, document.getElementById("root"));
 
-function Card(props){
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="avatra.img" />
-      <p>{props.tell}</p>
-      <p>{props.email}</p> 
-      
-    </div>
-  );
-}
-
-
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-    <Card 
-      name="Beyonce"
-      img ="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-
-      tell="+123 456 789"
-      email="b@beyonce.com"
-
-    />
-  <Card 
-      name="Jack Bauer"
-      img ="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-
-      tell="+123 456 789"
-      email="jack@nowhere.com"
-
-    />
-
-<Card 
-      name="Chuck Norris"
-      img ="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-
-      tell="+918 372 574"
-      email="gmail@chucknorris.com"
-
-    />
-  </div>,
-  document.getElementById("root")
-);
+//1. Apply CSS styles to App.jsx component
+//to match the appearance on the completed app:
+//https://c6fkx.csb.app/
+//2. Extract the contact card as a reusable Card component.
+//3. Use props to render the default Beyonce contact card
+//so the Card component can be reused for other contacts.
+//4. Import the contacts.js file to create card components.
